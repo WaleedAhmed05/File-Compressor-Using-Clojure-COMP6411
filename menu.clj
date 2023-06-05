619c5fa1f70e231199a2300ba7695bd15f8b2bd0
WaleedAhmed05 <waloo_waleed@yahoo.com>
2023-06-05 17:44:46 -0400

    Menu option 1- Display list of files working...

--- menu.clj
+++ menu.clj
@@ -1,7 +1,8 @@
 #_( Author -Waleed Ahmed05
            Version - 1.0)
 
-(ns file-compressor.menu)
+(ns file-compressor.menu
+  (:require [clojure.java.io :as io]))
 
 (defn main-menu []                                          ;Main menu to display options.
   (println "*** Compression Menu***
@@ -25,7 +26,46 @@
         (recur)))))
 
 (def user-input (get-user-input))
-;(println "Hello, " user-input "!")
+
+
+(defn display_list_of_files []
+  (println "File List: ")
+  (let [current-dir (io/file ".")]
+    (doseq [file (file-seq current-dir)
+            :when (.isFile file)
+            :let [filename (.getName file)]]
+      (println "* ./"filename))))
+
+  ;(let [current-dir (io/file ".")]
+  ;  (doseq [file (file-seq current-dir)
+  ;          :when (.isFile file)
+  ;          :let [filename (.getName file)]]
+  ;    (when (.endsWith filename ".txt")
+  ;      (println filename)))))
+
+
+
+
+(defn display_file_content [])                             ;TODO
+(defn compress_file [])                             ;TODO
+(defn decompress_file [])                             ;TODO
+(defn exit [])                             ;TODO
+
+
+(defn menu-options [n]
+  (case n
+    1 (display_list_of_files)                               ;Display list of files.
+    2 (display_file_content)                                ;Display content of a selected file.
+    3 (compress_file)                                       ;Compress file
+    4 (decompress_file)                                     ;UnCompress file back to original
+    5 (exit)                                                ;Close program.
+    (println "Invalid Argument")
+    )
+  )
+
+(menu-options 1)
+
+
 
 
 
