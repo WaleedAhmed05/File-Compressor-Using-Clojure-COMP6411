5bba6dd96e3aef8ae01fc1f4761e398d061308bb
WaleedAhmed05 <waloo_waleed@yahoo.com>
2023-06-12 03:27:39 -0400

    Compression and decompression of simple input files working

--- compress.clj
+++ compress.clj
@@ -1,34 +1,60 @@
 (ns file-compressor.compress
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as strX]))
 
 
-(defn read-frequenciesx [file-name]
+#_ "Decompression Procedure."
+;1.0
+;This method will read frequency.txt file and store all distinct words in a map.
+(defn read-words-from-frequencytxt [file-name]
   (with-open [reader (io/reader file-name)]
     (let [lines (line-seq reader)
-          ;words (map clojure.string/lower-case (clojure.string/split (clojure.string/join " " lines) #"\s"))
-          words (clojure.string/split (clojure.string/join " " lines) #"\s")
-          unique-words (distinct words)
-          word-frequencies (zipmap unique-words (range))]
+          ;words (map strX/lower-case (strX/split (strX/join " " lines) #"\s"))
+          words (strX/split (strX/join " " lines) #"\s")
+          unique-words (distinct words)                     ;store non duplicates only.
+          word-frequencies (zipmap unique-words (range))]   ;store each word as "key" and value as it's occurance.
       word-frequencies)))
 
-;(println (count (read-frequencies "frequency.txt")) )
-(def xVar (read-frequenciesx "frequency.txt"))
-;(println (count (read-frequenciesx "frequency.txt")))
+(def wordsMap (read-words-from-frequencytxt "frequency.txt")) ;store frequency map.
 
-;The pink elephant is absolutely groovy
-;The first man is from the last group of people
+;2.0
+;This method will read user input file to compress it's content.
+(defn read-input-file [file-name]
+  (with-open [reader (io/reader file-name)]
+    (let [lines (line-seq reader)
+          words (strX/split (strX/join " " lines) #"\s")
+          ]
+      words
+      )))
 
-(println (get xVar "the")
-         (get xVar "first")
-         (get xVar "man")
-         (get xVar "is")
-         (get xVar "from")
-         (get xVar "the")
-         (get xVar "last")
-         (get xVar "group")
-         (get xVar "of")
-         (get xVar "people")
-         )
+(def input-file-words-stream (read-input-file "2.txt"))     ;store input file words.
+
+(defn transform-input-file [words]
+  (mapv #(get wordsMap (strX/lower-case %)) words))         ;compressing input file words by mapping to it's number.
+
+
+(def store-compressed-results                               ;store compressed results.
+  (transform-input-file input-file-words-stream))
+
+(def output-file-name "output.txt")
+
+(defn save-words-to-file [words filename]                   ;store compressed words into output file.
+  (spit filename (strX/join " " words)))
+
+(save-words-to-file store-compressed-results "output.txt.ct")
+
+
+
+#_ "Decompression Procedure."
+(defn find-key-by-value [mapX value]
+(->> mapX
+     (filter #(= value (val %)))
+     (map key)
+     (first)))
+
+(println (find-key-by-value wordsMap 0))
+
+
 
 
 
